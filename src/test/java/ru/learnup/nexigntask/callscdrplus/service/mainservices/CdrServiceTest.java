e8923b9b91e93b3608de631a2d7594e1ff9bd06d
jbisssLT <pozolotusiko@mail.ru>
2023-04-28 14:12:41 +0300

    [codedrop] callCode test

--- src/test/java/ru/learnup/nexigntask/callscdrplus/service/mainservices/CdrServiceTest.java
+++ src/test/java/ru/learnup/nexigntask/callscdrplus/service/mainservices/CdrServiceTest.java
@@ -1,14 +1,20 @@
 package ru.learnup.nexigntask.callscdrplus.service.mainservices;
 
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions;
+import ru.learnup.nexigntask.callscdrplus.enums.CallCode;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.AnyOf.anyOf;
+import static org.hamcrest.core.Is.is;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 public class CdrServiceTest {
 
@@ -23,7 +29,30 @@ public class CdrServiceTest {
         Assertions.assertEquals(11, generateNumber().length());
     }
 
-    private String generateNumber(){
+    @Test
+    public void testCallCodeGeneration() {
+        CallCode resultCallCode = generateCallCode();
+        assertThat(resultCallCode, anyOf(is(CallCode.CALL_IN), is(CallCode.CALL_OUT)));
+    }
+
+    private CallCode generateCallCode() {
+        Method generateCallCode;
+        try {
+            generateCallCode = CdrService.class.getDeclaredMethod("generateCallType");
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        generateCallCode.setAccessible(true);
+        CallCode callCode;
+        try {
+            callCode = (CallCode) generateCallCode.invoke(cdrService);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+        return callCode;
+    }
+
+    private String generateNumber() {
         Method generateNumber;
         try {
             generateNumber = CdrService.class.getDeclaredMethod("generateNumber");
